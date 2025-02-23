d36cc97b7ef8231ee74551487db996ff9e3d55cf
Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2025-02-23 23:21:15 +0530

    Update App.tsx

--- my-task-app/src/App.tsx
+++ my-task-app/src/App.tsx
@@ -1,16 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import AuthRoute from "./components/AuthRoute";
+import {Outlet} from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<AuthRoute><Home /></AuthRoute>} />
-      </Routes>
-    </BrowserRouter>
+    <Outlet/>
   );
 };
 
