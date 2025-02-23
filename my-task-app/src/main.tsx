c142bf522a86dc3c4da12da03a6a8635dc840501
Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2025-02-23 23:21:23 +0530

    Update main.tsx

--- my-task-app/src/main.tsx
+++ my-task-app/src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {RouterProvider ,createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import AuthRoute from "./components/AuthRoute";
+
+const route=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+        <Route path="/" element={<AuthRoute><Home /></AuthRoute>} />
+        <Route path="login" element={<Login />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route}/>
   </StrictMode>,
 )
